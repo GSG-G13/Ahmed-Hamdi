88e585b08d061b95e037887714a61152a49ca155
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-03-29 09:42:30 +0200

    Create get one server route Function Relates #9 Closes #9

--- src/controller/users/users.js
+++ src/controller/users/users.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-console */
 const getAllUsers = (req, res) => {
-  fetch('https://api.github.com/uers')
+  fetch('https://api.github.com/users')
     .then((result) => result.json())
     .then((result) => res.status(200).json(result))
     .catch(console.log);
 };
-module.exports = { getAllUsers };
+// @TODO - refactor result.json
+const getOneUser = (req, res) => {
+  const { username } = req.params;
+  fetch(`https://api.github.com/users/${username}`)
+    .then((result) => result.json())
+    .then((result) => res.status(200).json(result))
+    .catch(console.log);
+};
+
+module.exports = { getAllUsers, getOneUser };
